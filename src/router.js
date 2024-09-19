3eba8db12fdbfa594c44d4898ad2d078c58c9010
hgkim1598 <hgkim1598@gmail.com>
2024-09-19 15:19:58 +0900

    FEAT 웹페이지 기본 타이틀 및 동적 타이틀 설정

--- src/router.js
+++ src/router.js
@@ -9,12 +9,14 @@ const routes = [
     {
         path: '/meet',
         name: 'Meet',
-        component: () => import("@/pages/meetnosh/Meet/index.vue")
+        component: () => import("@/pages/meetnosh/Meet/index.vue"),
+        meta: {title: '장소 정하기:MN'}
     },
     {
         path: '/nosh',
         name: 'Nosh',
-        component: () => import("@/pages/meetnosh/Nosh/index.vue")
+        component: () => import("@/pages/meetnosh/Nosh/index.vue"),
+        meta: {title: '맛집 찾기:MN'}
     },
 ]
 
